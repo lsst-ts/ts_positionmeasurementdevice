f0776f164502f4b277e4fce666bd409f9d2ec013
patrickingraham <pingraham@lsst.org>
2021-06-29 11:03:09 -0700

    pyserial doesn't raise a timeout exception, so replaced with an if statement.

--- python/lsst/ts/pmd/component.py
+++ python/lsst/ts/pmd/component.py
@@ -127,13 +127,14 @@ class MitutoyoComponent:
         self.log.debug(f"Message to be sent is {msg}")
         self.commander.write(f"{msg}\r".encode())
         self.log.debug("Message written")
-        try:
-            reply = self.commander.read_until(b"\r")
+
+        reply = self.commander.read_until(b"\r")
+        if reply != b"":
             self.log.debug(f"Read successful in send_msg, got {reply}")
             return reply
-        except TimeoutError:
+        else:
             reply = b"\r"
-            self.log.debug(f"Timed out on read in send_msg, returning {reply}")
+            self.log.warning(f"Timed out on read in send_msg, returning {reply}")
             return reply
 
     def get_slots_position(self):
